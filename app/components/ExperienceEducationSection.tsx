16352d1f9e83b93acab969437a03b9c9bfec6249
Mohammed-Hardi <abdulaimohammedhardi5@gmail.com>
2026-06-07 09:37:59 -0700

    Restore AWS education item

--- app/components/ExperienceEducationSection.tsx
+++ app/components/ExperienceEducationSection.tsx
@@ -7,13 +7,7 @@ const education = [
     degree: "BSc. Computer Science (Top-Up Degree)",
     institution: "University of Energy and Natural Resources (UENR)",
     location: "Sunyani, Ghana",
-    date: "Jan 2025 — Present",
-  },
-  {
-    degree: "Diploma in Computer Science",
-    institution: "University of Energy and Natural Resources (UENR)",
-    location: "Sunyani, Ghana",
-    date: "Jan 2022 — Nov 2024",
+    date: "Jan 2022 — Present",
   },
   {
     degree: "AWS re/Start Program",
